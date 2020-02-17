34cbee1ebc588394c869618e58da8240708dfe1d
Alexandre S. Soares <ale_santos.soares@hotmail.com>
2020-02-16 22:23:46 -0300

    chore: add export to components

---
+++ src/components/index.js
@@ -0,0 +1,19 @@
+import Badge from './Badge'
+import Banner from './Banner'
+import Button from './Button'
+import Card from './Card'
+import CardProduct from './Card/CardProduct'
+import CardSeller from './Card/CardSeller'
+import CreditCard from './CreditCard'
+import Title from './Title'
+
+export {
+  Badge,
+  Banner,
+  Button,
+  Card,
+  CardProduct,
+  CardSeller,
+  CreditCard,
+  Title,
+}
