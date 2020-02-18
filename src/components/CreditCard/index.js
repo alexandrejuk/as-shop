5dc349f75a4bb0b9d272a6044759f1ab645c364f
Alexandre S. Soares <ale_santos.soares@hotmail.com>
2020-02-18 15:59:20 -0300

    chore: update path some icons

--- src/components/CreditCard/index.js
+++ src/components/CreditCard/index.js
@@ -1,8 +1,8 @@
 import React, { memo } from 'react'
 import PropTypes from 'prop-types'
 import { Title } from '../'
-import ChipIcon from '../../assets/images/icons/chip.png'
-import TagIcon from  '../../assets/images/icons/tag.png'
+import ChipIcon from '../../assets/icons/chip.png'
+import TagIcon from  '../../assets/icons/tag.png'
 import styles from './style.module.css'
 
 const defaultValueCredidCard = {
