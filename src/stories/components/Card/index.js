ea5a7f302749115ac7d1b38a4c57c1cd1c06fe97
Alexandre S. Soares <ale_santos.soares@hotmail.com>
2020-02-18 20:51:23 -0300

    chore: update storybook

--- src/stories/components/Card/index.js
+++ src/stories/components/Card/index.js
@@ -14,9 +14,9 @@ const cardAction = action('item detail')
 const itemList = {
   amount: 1900,
   image: 'https://blog-cdn.eduk.com.br/wp-content/uploads/sites/3/2016/08/18130620/hamburguer_shutterstock.jpg',
-  title:"Lorem Ipsum is simply dummy",
+  title: "Lorem Ipsum is simply dummy",
   quantity: 10,
-  subtitle:"Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+  subtitle: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
   status: "PAGO",
   data: "10 jan",
   phone: "(11) 9 6503-5205",
