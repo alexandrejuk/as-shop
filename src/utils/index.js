8883ec152ee09ccda507798c05d32260cd30d94b
Alexandre S. Soares <ale_santos.soares@hotmail.com>
2020-02-18 17:31:28 -0300

    chore: add default value current

--- src/utils/index.js
+++ src/utils/index.js
@@ -9,13 +9,18 @@ const getInitiais = value => {
   return initials
 }
 
-const parserCurrentBR = value => {
+const parserCurrentBR = (value = 0) => {
+  if (value === 0) {
+    return `R$ 0,00`
+  }
+
   var parserArray = value.toString().split('')
   const size = parserArray.length
   const parserBR = [
     ...remove((size- 2), 2, parserArray),
     `,${parserArray[(size - 2)]}${parserArray[(size - 1)]}`,
   ].join('')
+
   return `R$ ${parserBR}`
 }
 
